2335be3569a6e3d2b6324c907fe950fa5c9fd447
royarnr <arnab.roy4@cognizant.com>
2017-07-02 17:28:07 +0530

    java2json

---
+++ JSONtoPOJO/src/Coverter.java
@@ -0,0 +1,81 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+public class Coverter {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Create JAVA POJO from JSON
+		 * Sample JSON file in the project directory,namely, SampleJSON.txt
+		 */
+
+		Gson gson = new Gson();
+
+		try(Reader reader = new FileReader(new File(System.getProperty("user.dir")+File.separator+"SampleJSON.txt")))
+		{
+			Identity identity = gson.fromJson(reader, Identity.class);
+			System.out.println(identity.getPerson().getage());
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace(System.err);
+			System.out.println("Conversion of JAVA POJO from JSON object failed");
+		}
+
+		/*
+		 * Create JSON from JAVA Objects
+		 */
+
+		/*
+		 * Instantiate the object and set its parameters
+		 */
+		List<String> skillset = new ArrayList<>();
+		Person person = new Person();
+		person.setname("Arnab");
+		person.setage(21);
+		skillset.add("Selenium");
+		skillset.add("Java");
+		skillset.add("VBScript");
+		person.setSkills(skillset);
+		
+		/*
+		 *Initialize parameters for the Address object 
+		 *embedded in Person object
+		 */
+		Address address = new Address();
+		address.setBuildingno("B-2");
+		address.setStreet1("Narkelbagan");
+		address.setStreet2("Garia");
+		address.setCity("Kolkata");
+		address.setPostalcode("700084");
+		address.setProvince("West Bengal");
+		
+		person.setAddress(address);
+
+		/*
+		 * Convert the JAVA Object to JSON and store it in project directory
+		 */
+		try(FileWriter writer = new FileWriter(new File(System.getProperty("user.dir")+File.separator+"ResultantJSON_"+new SimpleDateFormat("ddMMyyyy_HHmmss").format(new Date())+".txt")))
+		{
+			String jsonline = gson.toJson(person);
+			writer.write(jsonline);
+			System.out.println(jsonline);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace(System.err);
+			System.out.println("Conversion of JSON from JAVA object failed");
+		}
+
+	}
+
+}
